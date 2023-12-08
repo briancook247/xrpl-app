a89dbd11998324c431d8c245d1efb827b827d0c1
TechniqueIsKey <btcsg7@icloud.com>
2023-12-08 14:22:43 -0700

    Update Landing Page
    Added sections and fixed parallax effect.

--- xrpl-app/src/Components/HomePage.jsx
+++ xrpl-app/src/Components/HomePage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css'; // Import the CSS file
 
 const HomePage = () => {
   const parallaxRef = useRef(null);
@@ -6,92 +8,84 @@ const HomePage = () => {
   useEffect(() => {
     const handleScroll = () => {
       if (parallaxRef.current) {
-        const scrollValue = window.scrollY;
-        parallaxRef.current.style.backgroundPositionY = `${scrollValue * 0.5}px`; // Adjust the factor for desired speed
+        const offset = window.pageYOffset;
+        parallaxRef.current.style.transform = `translateY(${offset * 0.5}px)`;
       }
     };
 
     window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <div>
-      {/* Section 1 with parallax effect */}
-      <section
-        ref={parallaxRef}
-        style={{
-          backgroundImage: 'url("/BG1.png")', // Relative path from the root
-          backgroundSize: 'cover',
-          backgroundPosition: 'center center',
-          width: '100%',
-          height: '1000px', // Set the desired height
-          color: '#fff', // Text color for better contrast
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
+      {/* Parallax Section */}
+      <div className="parallax-wrapper">
+        <div ref={parallaxRef} className="parallax">
+          {/* Content of your parallax section */}
+          <div className="container">
+            <h1>Welcome to Our Platform</h1>
+            <p>Engage with the XRPL on Web 3.0</p>
+          </div>
+        </div>
+      </div>
+
+      {/* About Section */}
+      <section style={sectionStyle}>
         <div className="container">
-          <h1>Our Mission</h1>
-          <p>Free and Easy banking for all</p>
+          <h2>About</h2>
+          <p>This app leverages the XRPL (XRP Ledger) to showcase Web 3.0 capabilities. Users can seamlessly create a wallet, sign in, and interact with the XRP Ledger to mint, burn, and sell NFTs, all within their XRPL wallet account.</p>
         </div>
       </section>
 
-      {/* Section 2 */}
-      <section
-        style={{
-          backgroundColor: '#ffffff',
-          padding: '100px 0',
-          textAlign: 'center',
-        }}
-      >
+      {/* How to Use Section */}
+      <section style={{ ...sectionStyle, backgroundColor: '#f8f9fa' }}>
         <div className="container">
-          <h2>Our Services</h2>
-          <p>Explore our wide range of services designed to meet your business needs.</p>
+          <h2>How to Use</h2>
+          <p>Get started by creating your XRPL wallet. Sign in to access a suite of tools that enable you to create and manage NFTs directly on the blockchain.</p>
+          <Link to="/create-wallet" style={buttonStyle}>Get Started</Link>
         </div>
       </section>
 
-      {/* Section 3 */}
-      <section
-        ref={parallaxRef}
-        style={{
-          backgroundImage: 'url("/BG2.png")', // Relative path from the root
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          width: '100%',
-          height: '500px', // Set the desired height
-          color: '#fff', // Text color for better contrast
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
-      >
+      {/* Market Section */}
+      <section style={sectionStyle}>
         <div className="container">
-          <h2>Why Choose Us</h2>
-          <p>Discover why our clients trust us for their business solutions.</p>
+          <h2>NFT Market</h2>
+          <p>Explore our NFT marketplace where you can buy, sell, or trade digital assets securely on the XRPL, benefiting from the speed and efficiency of the XRP Ledger.</p>
+          <Link to="/market" style={buttonStyle}>Go to Market</Link>
         </div>
       </section>
 
-      {/* Section 4 */}
-      <section
-        style={{
-          backgroundColor: '#ffffff',
-          padding: '1000px 0',
-          textAlign: 'center',
-        }}
-      >
+      {/* Contact Us Section */}
+      <section style={{ ...sectionStyle, backgroundColor: '#f8f9fa' }}>
         <div className="container">
           <h2>Contact Us</h2>
-          <p>Get in touch with us to discuss your project or inquire about our services.</p>
+          <p>If you have any questions or need assistance, our team is here to help. Get in touch to learn more about how you can use our platform to your advantage.</p>
+          <Link to="/contact" style={buttonStyle}>Contact Us</Link>
         </div>
       </section>
     </div>
   );
+};
+
+const sectionStyle = {
+  padding: '100px 0',
+  textAlign: 'center',
+  backgroundColor: '#121212', // Dark theme for sections
+  color: '#fff',
+};
+
+const buttonStyle = {
+  backgroundColor: '#0d6efd', // Bootstrap primary button color for reference
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '5px',
+  fontSize: '1rem',
+  cursor: 'pointer',
+  textDecoration: 'none', // Remove underline from link
+  transition: 'background-color 0.3s',
+  margin: '20px 0', // Add some margin to top and bottom
 };
 
 export default HomePage;
--- xrpl-app/src/Components/NavBar.jsx
+++ xrpl-app/src/Components/NavBar.jsx
@@ -1,34 +1,28 @@
-// Navbar.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
+import './navbar.css'; // Import the CSS file
+
 
 const Navbar = () => {
   return (
-    <nav style={{ backgroundColor: '#333', color: '#fff', padding: '10px 20px' }}>
-      <ul
-        style={{
-          listStyle: 'none',
-          display: 'flex',
-          justifyContent: 'space-around',
-          padding: 0,
-        }}
-      >
+    <nav style={navbarStyle}>
+      <ul style={navListStyle}>
         <li>
-          <Link to="/home" style={navLinkStyles}>
+          <Link to="/home" style={navLinkStyles} className="nav-link">
             <FontAwesomeIcon icon={faHome} style={iconStyles} />
             Home
           </Link>
         </li>
         <li>
-          <Link to="/account" style={navLinkStyles}>
+          <Link to="/account" style={navLinkStyles} className="nav-link">
             <FontAwesomeIcon icon={faUser} style={iconStyles} />
             Account
           </Link>
         </li>
         <li>
-          <Link to="/market" style={navLinkStyles}>
+          <Link to="/market" style={navLinkStyles} className="nav-link">
             <FontAwesomeIcon icon={faShoppingCart} style={iconStyles} />
             Market
           </Link>
@@ -38,18 +32,35 @@ const Navbar = () => {
   );
 };
 
+const navbarStyle = {
+  backgroundColor: '#1a1a1a', // Dark background for the navbar
+  color: '#fff',
+  padding: '10px 20px',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+};
+
+const navListStyle = {
+  listStyle: 'none',
+  display: 'flex',
+  margin: 0,
+  padding: 0,
+};
+
 const navLinkStyles = {
   display: 'flex',
   alignItems: 'center',
   textDecoration: 'none',
-  padding: '10px 20px',
   color: '#fff',
-  borderRadius: '10px',
+  borderRadius: '5px',
+  padding: '10px 20px',
   transition: 'background-color 0.3s, transform 0.2s',
 };
 
 const iconStyles = {
   marginRight: '8px',
+  color: '#61dafb', // Vibrant blue for icons
 };
 
 export default Navbar;
